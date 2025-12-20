5facc0ee4523d07b1613d31272dd2e21f62aae37
Anna Polisciuc <annap00775@gmail.com>
2025-12-20 14:55:00 +0200

    feat: auth, sessions, private notes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,7 +5,7 @@ export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1)*perPage;
 
-  const notesQuery = Note.find();
+  const notesQuery = Note.find({ userId: req.user._id });
 
   if (tag) {
     notesQuery.where("tag").equals(tag);
@@ -32,68 +32,58 @@ export const getAllNotes = async (req, res) => {
 
 
 export const getNoteById = async (req, res, next) => {
-  try {
-
     const { noteId } = req.params;
-    const note = await Note.findById(noteId);
-
+    const note = await Note.findOne({
+      _id: noteId,
+      userId: req.user._id,
+    });
     if (!note) {
-      next(createHttpError(404, 'Note not found'));
-      return;
+      throw createHttpError(404, "Note not found");
+
     }
     res.status(200).json(note);
-  } catch (error) {
-    next(error);
-  }
 };
 
 
-export const createNote = async (req, res, next) => {
-  try {
-  const note = await Note.create(req.body);
+export const createNote = async (req, res) => {
+  const note = await Note.create({
+    ...req.body,
+  userId: req.user._id,
+  });
   res.status(201).json(note);
-  } catch (error) {
-    next(error);
-  }
 };
 
 
 export const deleteNote = async (req, res, next) => {
-  try {
+
   const { noteId } = req.params;
   const note = await Note.findOneAndDelete({
     _id: noteId,
+    userId: req.user._id,
   });
 
   if (!note) {
-    next(createHttpError(404, "Note not found"));
-    return;
+   throw createHttpError(404, "Note not found");
   }
 
   res.status(200).json(note);
-} catch (error) {
-  next(error);
-}
 };
 
 export const updateNote = async (req, res, next) => {
-  try {
 
   const { noteId } = req.params;
   const note = await Note.findOneAndUpdate(
-    { _id: noteId },
+    { _id: noteId,
+      userId: req.user._id,
+    },
     req.body,
     { new: true },
   );
 
   if (!note) {
-    next(createHttpError(404, 'Note not found'));
-    return;
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
-} catch (error) {
-  next(error);
-}
 };
 
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import { errors } from "celebrate";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(logger);
 
 app.use(notesRoutes);
@@ -31,3 +33,5 @@ await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
